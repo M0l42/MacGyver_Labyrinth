360962b8543d5a9a95d0d6c1f27fd96a1618b397
M042 <boukobza.nathan@gmail.com>
2019-04-21 14:14:53 +0200

    Creating class

---
+++ main.py
@@ -0,0 +1,23 @@
+class Map:
+    def __init__(self):
+        pass
+
+    def generate_map(self):
+        pass
+
+
+class Character:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.alive = True
+        pass
+
+
+class MacGyver(Character):
+    def move(self):
+        pass
+
+
+class Items:
+    pass
