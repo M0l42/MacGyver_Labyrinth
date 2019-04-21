06442ab32a11433f418de23d8795e4011cf36fd5
M042 <boukobza.nathan@gmail.com>
2019-04-21 15:14:54 +0200

    Initialize the map and the characters

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import pygame
 
 class Map:
     def __init__(self):
-        pass
-
-    def generate_map(self):
-        pass
+        self.case = [[0] * 15 for i in range(15)]
+        self.surface = pygame.image.load("pictures/labyrinthe.png").convert()
 
 
 class Character:
-    def __init__(self, x, y):
+    def __init__(self, x, y, picture):
         self.x = x
         self.y = y
         self.alive = True
-        pass
+        self.surface = pygame.image.load("pictures/" + picture).convert()
 
 
 class MacGyver(Character):
@@ -29,7 +27,10 @@ class Items:
 def main():
     pygame.init()
 
-    pygame.key.set_repeat(10,10)
+    pygame.key.set_repeat(10, 10)
+
+    macGyver = MacGyver(0, 0, "MacGyver.png")
+    guard = Character(14, 14, "Gardien.png")
 
     pygame.quit()
 
