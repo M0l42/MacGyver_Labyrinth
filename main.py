703f0d3e6a4cd6fdc8db95ca890b8342657fb4fd
M042 <boukobza.nathan@gmail.com>
2019-04-21 16:56:16 +0200

    Configuring the move method

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
 
+white = (255,255,255)
 
 class Map:
+    # Loading the map
     def __init__(self):
-        self.case = [[0] * 15 for i in range(15)]
         self.surface = pygame.image.load("pictures/labyrinthe.png").convert()
 
 
 class Character:
+    # Initialization of the character
     def __init__(self, x, y, picture):
         self.x = x
         self.y = y
@@ -16,8 +18,20 @@ class Character:
 
 
 class MacGyver(Character):
-    def move(self):
-        pass
+    # How MacGyver will move depending on the keys pressed
+    def move(self, keystate, labyrinth, case_length):
+        if keystate[pygame.K_UP]:
+            if labyrinth.surface.get_at((self.y*case_length+15, self.x*case_length)) == white:
+                self.x -= 1
+        if keystate[pygame.K_DOWN]:
+            if labyrinth.surface.get_at((self.y*case_length+15, self.x*case_length + case_length)) == white:
+                self.x += 1
+        if keystate[pygame.K_RIGHT]:
+            if labyrinth.surface.get_at((self.y*case_length + case_length, self.x*case_length+15)) == white:
+                self.y += 1
+        if keystate[pygame.K_LEFT]:
+            if labyrinth.surface.get_at((self.y*case_length, self.x*case_length+15)) == white:
+                self.y -= 1
 
 
 class Items:
@@ -25,8 +39,9 @@ class Items:
 
 
 def main():
-    keep_playing = True
+    keep_playing = True  # It will be true until the player win or die
     case_length = 40
+
     pygame.init()
 
     pygame.key.set_repeat(10, 10)
@@ -38,14 +53,18 @@ def main():
     guard = Character(15, 15, "Gardien.png")
 
     while keep_playing:
+        # bliting the all the image to the screen
         screen.blit(labyrinth.surface, (0, 0))
         screen.blit(macgyver.surface, (macgyver.y*case_length+3, macgyver.x*case_length+3))
         screen.blit(guard.surface, (guard.y * case_length + 3, guard.x * case_length + 3))
+        # Displaying the new screen
         pygame.display.flip()
+        # Getting the value of the key pressed by the player
         keystate = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keep_playing = False
+            macgyver.move(keystate, labyrinth, case_length)
 
     pygame.quit()
 
