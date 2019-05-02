28a7bf8c04052ca1ad0a84fe87ebf5544a9e2917
M042 <boukobza.nathan@gmail.com>
2019-05-02 15:46:00 +0200

    Adjust the win condition

--- macgyver_game/events/events.py
+++ macgyver_game/events/events.py
@@ -2,26 +2,23 @@ import pygame
 
 
 def quitting(macgyver, guard):
+    keep_playing = True
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return False
+            keep_playing = False
         else:
-            return True
+            keep_playing = True
     if (macgyver.x, macgyver.y) == (guard.x, guard.y):
         if macgyver.items_picked == 3:
             guard.alive = False
-            return True
+            macgyver.win = True
+            print("You win !")
+            keep_playing = False
         else:
             macgyver.alive = False
             print("You loose !")
-            return False
-    if macgyver.y == 16:
-        macgyver.win = True
-        print("You win !")
-        return False
-    else:
-        return True
-
+            keep_playing = False
+    return keep_playing
 
 def keyboard():
     return pygame.key.get_pressed()
